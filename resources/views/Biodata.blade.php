ff35163d8a2a0713eccb960e385fe4fa495cae41
LAPTOP-GIUH8V1K\LENOVO <rmlh1405@gmail.com>
2021-10-26 02:21:17 +0800

    selamat pagi

---
+++ resources/views/Biodata.blade.php
@@ -0,0 +1,23 @@
+<html>
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1 align="center">BIODATA DIRI</h1>
+       
+            <td>Nama</td>
+            <td>Ramlah Sari</td>
+       
+            <td>Kelas</td>
+            <td>B</td>
+        
+            <td>NIM</td>
+            <td>60900119046</td>
+        
+            <td>Email</td>
+            <td>60900119046@uin-alauddin.ac.id</td>
+</body>
+
+</html>
+
+</html>
